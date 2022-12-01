ab03e0486f24a70dbed963cd56985461c86d5471
faisalill <mfaisal.ilyas.d@gmail.com>
2022-12-01 23:33:27 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,89 @@
+# Dynamically typed
+# multiple assignments
+n, m = 0, "abc"
+a , b,c = 0.34, "workin", False
+
+# Increment
+n = n+1 
+n += 1 
+#  n++ does not work
+
+# None is null in python
+n = None
+
+# If statements
+n = 1 
+# indentation does to work instead of
+# curly braces
+if n > 2:
+    n = 3
+elif n == 2:
+     n *= 2
+else: 
+    n = 2             
+# multi line conditions 
+# and and or are used
+if ((n>2 and n!=m) or n==m):
+    n=3
+    
+# Loops
+# While
+while n < 5:
+    print(n)  
+# For 
+for i in range(5):
+    print(i) 
+    # prints till 5 with increment value of 1
+for i in range(1,6):
+    print(i)
+    # prints from 1 to 5 excluding 6
+for i in  range(1, 7, 5):
+    print(i)
+    # prints 1 and 7 with the increment of 5
+    
+# Division
+# for decimal division
+print(5/2)
+# for rounded division
+print(5//2)
+# negative numbers will be rounded down to the least number
+print(-3//2)
+# to round negative numbers to higher number
+print(int(-3/2))
+
+# Modulus
+print(10%3)
+# negative mods again round down to the least value 
+# rather than the highest one
+# to be consistent use math module functions
+
+# math helpers
+import math
+# modulus
+print(math.fmod(-10,3))
+# more math helpers
+# round down 
+print(math.floor(3/2))
+# round down 
+print(math.ceil(4/3))
+# square root
+print(math.sqrt(2))
+# power
+print(math.pow(2, 3))
+
+# max int
+float("inf")
+# min int
+float("-inf")
+
+# Lists aka Arrays
+arr = [1,2,3]
+# to add a num to the last
+arr.append(4)
+# to remove the last num
+arr.pop()
+# to add in the start
+arr.insert(1,7)
+
+# Initialising an array of size n with the value of 1
+arr = [1] * 5
