4aa398a4e393832f847d9a92a9ce11b7e21da22c
Faisal I <118456685+faisalill@users.noreply.github.com>
2022-12-10 23:30:45 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -237,3 +237,54 @@ print(arr[0][0], arr[3][3])
 # This won't work as you expect it to
 
 arr = [[0] * 4] * 4
+
+
+# Strings are similar to arrays
+
+s = "abc"
+
+print(s[0:2])
+
+>>> "ab"
+
+# But they are immutable, this won't work
+
+s[0] = "A"
+
+# This creates a new string
+
+s += "def"
+
+print(s)
+
+>>> "abcdef"
+
+# Valid numeric strings can be converted
+
+print(int("123") + int("123"))
+
+>>> 246
+
+# And numbers can be converted to strings
+
+print(str(123) + str(123))
+
+>>> "123123"
+
+# In rare cases you may need the ASCII value of a char
+
+print(ord("a"))
+
+print(ord("b"))
+
+>>> 97
+
+>>> 98
+
+# Combine a list of strings (with an empty string delimitor)
+
+strings = ["ab", "cd", "ef"]
+
+print("".join(strings))
+
+>>> "abcdef"
