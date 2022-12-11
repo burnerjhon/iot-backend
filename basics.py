d413da892ae20fbf68454d32270ac2b9d860850d
Faisal I <118456685+faisalill@users.noreply.github.com>
2022-12-11 22:34:05 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,80 +1,118 @@
-# Dynamically typed
-# multiple assignments
+# Variables are dynamicly typed
+n = 0
+print('n =', n)
+>>> n = 0
+
+n = "abc"
+print('n =', n)
+>>> n = abc
+
+# Multiple assignments
 n, m = 0, "abc"
-a , b,c = 0.34, "workin", False
+n, m, z = 0.125, "abc", False
 
 # Increment
-n = n+1 
-n += 1 
-#  n++ does not work
+n = n + 1 # good
+n += 1    # good
+n++       # bad
 
-# None is null in python
+# None is null (absence of value)
+n = 4
 n = None
+print("n =", n)
+>>> n = None
 
-# If statements
-n = 1 
-# indentation does to work instead of
-# curly braces
+# If statements don't need parentheses 
+# or curly braces.
+n = 1
 if n > 2:
-    n = 3
+    n -= 1
 elif n == 2:
-     n *= 2
-else: 
-    n = 2             
-# multi line conditions 
-# and and or are used
-if ((n>2 and n!=m) or n==m):
-    n=3
-    
-# Loops
-# While
+    n *= 2
+else:
+    n += 2
+
+# Parentheses needed for multi-line conditions.
+# and = &&
+# or  = ||
+n, m = 1, 2
+if ((n > 2 and 
+    n != m) or n == m):
+    n += 1
+n = 0
 while n < 5:
-    print(n)  
-# For 
+    print(n)
+    n += 1
+>>> 0 1 2 3 4
+
+# Looping from i = 0 to i = 4
 for i in range(5):
-    print(i) 
-    # prints till 5 with increment value of 1
-for i in range(1,6):
     print(i)
-    # prints from 1 to 5 excluding 6
-for i in  range(1, 7, 5):
+>>> 0 1 2 3 4
+
+# Looping from i = 2 to i = 5
+for i in range(2, 6):
     print(i)
-    # prints 1 and 7 with the increment of 5
+>>> 2 3 4 5
+
+# Looping from i = 5 to i = 2
+for i in range(5, 1, -1):
+    print(i)
+>>> 5 4 3 2
     
-# Division
-# for decimal division
-print(5/2)
-# for rounded division
-print(5//2)
-# negative numbers will be rounded down to the least number
-print(-3//2)
-# to round negative numbers to higher number
-print(int(-3/2))
+# Division is decimal by default
+print(5 / 2)
+>>> 2.5
 
-# Modulus
-print(10%3)
-# negative mods again round down to the least value 
-# rather than the highest one
-# to be consistent use math module functions
+# Double slash rounds down
+print(5 // 2)
+>>> 2
 
-# math helpers
+# CAREFUL: most languages round towards 0 by default
+# So negative numbers will round down
+print(-3 // 2)
+>>> -2
+
+# A workaround for rounding towards zero
+# is to use decimal division and then convert to int.
+print(int(-3 / 2))
+>>> -1
+
+# Modding is similar to most languages
+print(10 % 3)
+>>> 1
+
+# Except for negative values
+print(-10 % 3)
+>>> 2
+
+# To be consistent with other languages modulo
 import math
-# modulus
-print(math.fmod(-10,3))
-# more math helpers
-# round down 
-print(math.floor(3/2))
-# round down 
-print(math.ceil(4/3))
-# square root
-print(math.sqrt(2))
-# power
-print(math.pow(2, 3))
+from multiprocessing import heap
+print(math.fmod(-10, 3))
+>>> -1
 
-# max int
+# More math helpers
+print(math.floor(3 / 2))
+>>> 1
+print(math.ceil(3 / 2))
+>>> 2
+print(math.sqrt(2))
+>>> 1.4142135623730951
+print(math.pow(2, 3))
+>>> 8
+
+# Max / Min Int
 float("inf")
-# min int
 float("-inf")
+
+# Python numbers are infinite so they never overflow
+print(math.pow(2, 200))
+>>> 1.6069380442589903e+60
+
+# But still less than infinity
+print(math.pow(2, 200) < float("inf"))
+>>> True
 
 # Lists aka Arrays
 arr = [1,2,3]
