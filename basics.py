dbd11c37349ed6c00897472e07cf47add67297a7
Faisal I <118456685+faisalill@users.noreply.github.com>
2022-12-17 22:43:31 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -376,3 +376,17 @@ print(2 in mySet)
 >>> True
 print(3 in mySet)
 >>> False
+
+
+mySet.remove(2)
+print(2 in mySet)
+>>> False
+
+# list to set
+print(set([1, 2, 3]))
+>>> {1, 2, 3}
+
+# Set comprehension
+mySet = { i for i in range(5) }
+print(mySet)
+>>> {0, 1, 2, 3, 4}
